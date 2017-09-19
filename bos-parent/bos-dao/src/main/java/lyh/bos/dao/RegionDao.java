bb2ab9a135c477b3647a79f26c55d3a7527576f2
lyh <380056867@qq.com>
2017-09-19 14:52:45 +0800

    删除

--- bos-parent/bos-dao/src/main/java/lyh/bos/dao/RegionDao.java
+++
@@ -1,8 +0,0 @@
-package lyh.bos.dao;
-
-import lyh.bos.base.dao.IBaseDao;
-import lyh.bos.domain.Region;
-
-public interface RegionDao extends IBaseDao<Region>{
-
-}
--- bos-parent/bos-dao/src/main/java/lyh/bos/dao/StaffDao.java
+++
@@ -1,9 +0,0 @@
-package lyh.bos.dao;
-
-import lyh.bos.base.dao.IBaseDao;
-import lyh.bos.domain.Staff;
-
-public interface StaffDao extends IBaseDao<Staff>{
-    
-
-}
